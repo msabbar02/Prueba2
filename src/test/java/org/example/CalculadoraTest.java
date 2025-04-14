25ba453d30578f52a5f3fd781bba47b4c861ac24
msabbar02 <msabbar1@educacion.navarra.es>
2025-04-14 02:00:18 +0200

    añadir la clase CalculadoraTest

---
+++ src/test/java/org/example/CalculadoraTest.java
@@ -0,0 +1,45 @@
+package org.example;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class CalculadoraTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void suma() {
+    }
+
+    @Test
+    void resta() {
+    }
+
+    @Test
+    void multiplicacion() {
+    }
+
+    @Test
+    void division() {
+    }
+
+    @Test
+    void esPar() {
+    }
+
+    @Test
+    void maximo() {
+    }
+
+    @Test
+    void esDivisible() {
+    }
+}
